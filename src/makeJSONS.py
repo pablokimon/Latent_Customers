8c98d1813d117ccea7dffc8e90b75f4a2cf05329
Paul Knowles <Sarah@Sorry-III-2.local>
2019-04-05 15:06:39 -0700

    add img

--- src/makeJSONS.py
+++ src/makeJSONS.py
@@ -1,9 +1,13 @@
 import re
 import json
+import os
 
 def pos_float(s):
+    #print (s)
     if s[-1]=="-":
         return -float(s[:-1])
+    elif s[-1]=='+':
+        return float(s[:-1])
     else:
         return float(s)
 
@@ -13,11 +17,14 @@ def parse_entry(entry):
     if match is None:
         return None
     transinfo = match.groups()
-    
-    item_re = r"^( \w+)?\s+([\w \-,\+.'\"\/%&]+?)\s+(\d+\.\d+-?)\s+\w?\s+Dept\s+(\d+)"
+    if transinfo[8]!='Checkout':
+        return None
+    item_re = r"^ (\w+)?\s+([\w \-,\+.'\"\\\/%&]+?)\s+(\d+\.\d+-?)\s+\w?\s+Dept\s+(\d+)"
     items = re.findall( item_re, entry, flags=re.MULTILINE )
+    
     account_re = r"\s[Account]+\s+\d+$"
     account = re.findall(account_re,entry,flags=re.MULTILINE )
+    
     ret = {'date':transinfo[0],
            'time':transinfo[1],
            'term':int(transinfo[2]),
@@ -27,8 +34,8 @@ def parse_entry(entry):
            'gross-':pos_float(transinfo[6]),
            'net':pos_float(transinfo[7]),
            'type':transinfo[8],
-           'account':account}
-    
+          'account':account}
+    #[[d[-4:] for d in account]]
     items = [(x[0].strip(), x[1].strip(), pos_float(x[2]), int(x[3])) for x in items]
     
     ret['items'] = items
@@ -66,15 +73,43 @@ def pos_to_json(fn_in, fn_out):
         fpout.write("\n")
     fpout.close()
 
-if __name__=='__main__':
-    import sys
-    
-    if len(sys.argv)<3:
-        print( "usage: %s transaction_filename output_filename"%sys.argv[0] )
-        exit()
-    
-    fn_in = sys.argv[1]
-    fn_out = sys.argv[2]
-    
-    pos_to_json(fn_in, fn_out)
-    
+
+
+for subdir, dirs, files in os.walk('./tlogs'):
+    for file in files:
+        #print os.path.join(subdir, file)
+        filepath = subdir + os.sep + file
+
+        if filepath.endswith("*log.txt"):
+            pass
+            #print (filepath)
+
+
+#if __name__ == '__main__':
+
+json_path ='./data/jsons/2017'
+tlog_path='./data/tlogs/2017'
+
+if not os.path.exists(tlog_path):
+    print("Directory " , tlog_path ,  "Does not exist")
+if not os.path.exists(json_path):
+    os.makedirs(json_path)
+    print("Directory " , json_path ,  " Created ")
+else:    
+    print("Directory " , json_path ,  " already exists")    
+
+
+for i,f in enumerate(os.walk(tlog_path)):
+    #print (i,f)
+    for ff in f:
+        
+        #print("ff:",ff)
+        if type(ff)!='list':
+            if str(ff).startswith(tlog_path):
+                path = ff
+        for fff in ff:
+            #print("fff:",fff)
+            if fff=="transactionlog.txt":
+                #print(path+'/'+fff)
+                #print (json_path+'/'+'%d.json'%i)
+                pos_to_json(path+'/'+fff,json_path+'/'+'%d.json'%i)
