f02e280984756050ed89db71466695f54bd5b93f
Paul Knowles <Sarah@Sorry-III-2.local>
2019-04-09 13:47:01 -0700

    final push

--- src/json_to_dataframe.py
+++
@@ -1,7 +0,0 @@
-def json_to_df(path):
-
-    #path = '/Users/Sarah/galvanize/dsi-capstone/jsons/'
-    all_files = glob.glob(os.path.join(path, "*.json"))
-    df = pd.concat((pd.read_json(f,keep_default_dates=False,lines=True) for f in all_files)) 
-
-    return df
